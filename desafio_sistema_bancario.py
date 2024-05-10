f0a097d8a3f36cdd648930af413233ca59aef9d5
Kaike Fonseca <kaikextreme3@gmail.com>
2024-05-10 17:41:11 -0300

    Versão 1.1 do sistema bancário

--- desafio_sistema_bancario.py
+++ desafio_sistema_bancario.py
@@ -27,7 +27,7 @@ while True:
             continue
         
         saldo += valor_deposito #INCRIMENTA VAL DEPOSITO NO SAQUE
-        print(f"Depósito realizado com sucesso.\nSaldo: {saldo}")
+        print(f"Depósito realizado com sucesso.\nSaldo: R$ {saldo:.2f}")
         extrato += f"O valor depositado foi de R$ {valor_deposito:.2f} \n" #FAZ UM 'LOG' NO EXTRATO
 
     elif opcao.upper() == "S": 
@@ -44,20 +44,25 @@ while True:
         if valor_saque > saldo:
             print("Saldo insuficiente!")
             continue
+        
+        if valor_saque >= 0: #verifica se o valor não é 0 ou negativo
+            numero_saques += 1 #incrementa 1 no contador do saque diario
 
-        numero_saques += 1 #incrementa 1 no contador do saque diario
+            saldo -= valor_saque #retirar o valor do saque do saldo da conta
 
-        saldo -= valor_saque #retirar o valor do saque do saldo da conta
+            print(f"\n{numero_saques}º do dia realizado com sucesso!\nSaldo: R$ {saldo:.2f}")  #expoe para o usuario
 
-        print(f"\n{numero_saques}º do dia realizado com sucesso!\nSaldo: {saldo}")  #expoe para o usuario
-
-        extrato += f"Saque: {numero_saques}. Valor sacado: R$ {valor_saque:.2f}\n" #armazena no log do extrato
+            extrato += f"Saque: {numero_saques}. Valor sacado: R$ {valor_saque:.2f}\n" #armazena no log do extrato
+        else:
+            print("O valor não pode ser 0 ou negativo.")
+            continue
     
     elif opcao.upper() == "E":
-        if len(extrato) > 0:
-            print(f"\n{extrato}Saldo: {saldo}")
-        else:
-            print(f"Não foram realizadas movimentações.\n")
+
+        print("\n=============== EXTRATO ===============")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}\n")
+        print("========================================")
     
     elif opcao.upper() == "Q":
         print("Obrigado por usar nosso sistema!")
